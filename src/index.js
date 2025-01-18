dd211c367551445f45580a141b9f81b070e36c75
jldec <jurgen@jldec.me>
2025-01-18 07:37:29 -0800

    implement echo and cleanup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		return Response.json({
+			time: new Date(),
+			url: request.url,
+			method: request.method,
+			body: await request.text(),
+			city: request.cf.city,
+			country: request.cf.country,
+			headers: Object.fromEntries(request.headers),
+		});
 	},
 };
